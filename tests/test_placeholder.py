24d9111d7918a39a59a352e29c76d461c35da102
Webb Scales <wscales@redhat.com>
2024-12-04 16:22:04 -0500

    Initial cut at a CI

---
+++ tests/test_placeholder.py
@@ -0,0 +1,12 @@
+import unittest
+
+# from src/toolname import testfunction
+
+
+class MyTestCase(unittest.TestCase):
+    def test_placeholder_test(self):
+        self.assertNotEqual(True, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
