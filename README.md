a1d67c13126260b62babf28a6a3035044b932f54
Rodolfo Napoles <rnapoles@rnapoles1-mac.home>
2024-12-05 16:40:44 -0500

    addressed feedback ncluding use of YAML

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 This project exports Logilica data to add it to a weekly report. 
 
-The data is obtained from team dashbaords, one dashboard per team. The list of dashboards is stored in the team_dashbaords.txt file. As teams are onboarded a new team dashboard is created in Logilica and the team_dashboards.txt file is updated with the new team's dashboard name.
+Environment variables must be set for LOGILICA_DOMAIN, LOGILICA_EMAIL, and
+LOGILICA_PASSWORD, the corresponding values can be obtained from Bitwarden.
 
-The download_pdf.py script uses Playwright, see https://playwright.dev/python/docs/intro for instructions to install Playwright. 
+The data is obtained from team dashboards, one dashboard per team. Teams data 
+is stored in the weekly_report.yaml file. 
 
-To run the script run `pytest download_pdfs.py`. To run in debug mode run `PWDEBUG=1 pytest download_pdfs.py`. For mode details on runnin and debugging see https://playwright.dev/python/docs/running-tests.
+When a new team is onboarded, a new dashboard should be created for it, and the 
+weekly_report.yaml file is updated accordingly.
+
+The download_pdf.py script uses Playwright, to setup your environmemt you can 
+run `pip install -r requirements.txt` 
+
+To run the script run `python3 download_pdfs.py`. To run in debug mode run 
+`PWDEBUG=1 python3 download_pdfs.py`. For mode details on running and debugging 
+see https://playwright.dev/python/docs/running-tests.
 
 The downloaded PDF files are stored in the `downloaded_pdfs` directory. 
+
+When a new team is onboarded, a new dashboard should be created for it, and 
+the dashboard name should be added to the file.
--- download_pdfs.py
+++ download_pdfs.py
@@ -1,24 +1,22 @@
-import os
+import os, yaml
+
 from playwright.sync_api import Playwright, sync_playwright
+
+
 def run(playwright: Playwright) -> None:
-    # Read list of tems dashboards and add tehm to dashboards
-    with open('teams_dashboards.txt') as data_file:
-        dashboards = data_file.readlines()
+    #Read and load weekly_report.yaml file
+    with open("weekly_report.yaml", "r")as yaml_file:
+        teams_data = yaml.safe_load(yaml_file)
 
     # Specify the download directory
-    download_path = "downloaded_pdfs/"
+    download_path = teams_data["config"]["Download_path"]
 
-    browser = playwright.chromium.launch(headless=False
-    )
-
-    context = browser.new_context(
-        accept_downloads=True,
-    )
-
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(accept_downloads=True)
     page = context.new_page()
 
     # Navigate to the login page
-    page.goto("https://logilica.io/login?from=Lw==")
+    page.goto("https://logilica.io/login")
 
     # Fill the login form using environment variables
     page.get_by_role("button", name="Log in With Email").click()
@@ -27,27 +25,30 @@ def run(playwright: Playwright) -> None:
     page.locator("#email").click()
     page.locator("#email").fill(os.getenv("LOGILICA_EMAIL"))
     page.locator("#password").click()
-    page.locator("#password").fill(os.getenv("LOGILICA_TOKEN"))
+    page.locator("#password").fill(os.getenv("LOGILICA_PASSWORD"))
 
     # Submit the form
     page.get_by_role("button", name="Login").click()
     page.get_by_role("link", name="Custom Reports").click()
 
     # Cycle though list of teams dashboards to download a pdf for each team
-    for dashboard in dashboards:
-        page.get_by_role("link", name=dashboard).click()
-        page.get_by_role("button", name="Export PDF").click()
-        # Handle download
-        with page.expect_download() as download_info:
-            page.get_by_role("button", name="Download").click()
-        download = download_info.value
-        download.save_as(download_path + download.suggested_filename)
-        # Close PDF view dialog
-        page.get_by_role("button", name="Close").click()
+    for team, dashboards in teams_data["teams"].items():
+        for dashboard, options in dashboards["team_dashboards"].items():
+            print("loop")
+            page.get_by_role("link", name=dashboard).click()
+            page.get_by_role("button", name="Export PDF").click()
+            # Handle download
+            with page.expect_download() as download_info:
+                page.get_by_role("button", name="Download").click()
+            download = download_info.value
+            download.save_as(download_path + options["Filename"])
+            # Close PDF view dialog
+            page.get_by_role("button", name="Close").click()
 
     # Clean up
     context.close()
     browser.close()
+
 
 with sync_playwright() as playwright:
     run(playwright)
