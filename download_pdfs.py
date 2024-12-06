072a55bffb95ab724ffaa394a45d298cb22e2421
Rodolfo Napoles <rnapoles@rnapoles1-mac.home>
2024-12-06 16:00:19 -0500

    incorporated additional feedback

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,21 +1,19 @@
-import os, yaml
+import os
 
+import yaml
 from playwright.sync_api import Playwright, sync_playwright
 
 
 def run(playwright: Playwright) -> None:
-    #Read and load weekly_report.yaml file
-    with open("weekly_report.yaml", "r")as yaml_file:
+    with open("weekly_report.yaml", "r") as yaml_file:
         teams_data = yaml.safe_load(yaml_file)
 
-    # Specify the download directory
     download_path = teams_data["config"]["Download_path"]
 
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(accept_downloads=True)
     page = context.new_page()
 
-    # Navigate to the login page
     page.goto("https://logilica.io/login")
 
     # Fill the login form using environment variables
@@ -24,7 +22,6 @@ def run(playwright: Playwright) -> None:
     page.locator("#email").fill(os.getenv("LOGILICA_EMAIL"))
     page.locator("#password").fill(os.getenv("LOGILICA_PASSWORD"))
 
-    # Submit the form
     page.get_by_role("button", name="Login").click()
     page.get_by_role("link", name="Custom Reports").click()
 
