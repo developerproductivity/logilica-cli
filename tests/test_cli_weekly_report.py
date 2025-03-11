01b270b2f970b853489fadd9a2528bd11d6d05f6
Karel Piwko <kpiwko@redhat.com>
2025-03-11 17:53:02 +0100

    feat!: DPROD-678 use docling to parse pdfs
    Additional options to parse documents.
    
    Breaking change:
    * `html` output now dump content to file using docling
    * use `console` for previous behavior
    
    Also introduced `scale` argument (float) to be able to change the DPI of images

--- tests/test_cli_weekly_report.py
+++ tests/test_cli_weekly_report.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import shutil
 
 from click.testing import CliRunner
+import pytest
 import yaml
 
 from logilica_weekly_report.__main__ import cli
@@ -9,8 +10,29 @@ from logilica_weekly_report.__main__ import cli
 with open(Path(__file__).parent / "fixtures/config.yaml", "r") as yaml_file:
     FULL_CONFIG = yaml.safe_load(yaml_file)
 
+OUTPUT_DIR = "./test-output"
 
-def test_weekly_report_image_generation():
+BASE_ARGS = [
+    "--username",
+    "testuser",
+    "--password",
+    "testpassword",
+    "--domain",
+    "testorg",
+    "--config",
+    "config.yaml",
+    "--output-dir",
+    OUTPUT_DIR,
+    "weekly-report",
+    "-t",
+    "downloads",
+    "-I",
+    "local",
+]
+
+
+@pytest.fixture(autouse=True)
+def setup_cli_isolated_env():
     runner = CliRunner()
     fixtures_dir = Path(__file__).parent / "fixtures"
 
@@ -20,29 +42,120 @@ def test_weekly_report_image_generation():
 
         target_dir = Path(".") / "downloads"
         shutil.copytree(fixtures_dir, target_dir)
+        yield runner
 
-        result = runner.invoke(
-            cli,
-            [
-                "-u",
-                "testuser",
-                "-p",
-                "testpassword",
-                "-d",
-                "testorg",
-                "-C",
-                "config.yaml",
-                "weekly-report",
-                "-t",
-                "downloads",
-                "-I",
-                "local",
-                "-O",
-                "images-only",
-            ],
-        )
 
-        assert result.exit_code == 0, result.output
-        assert Path(
-            "output/my-awesome-team-team-productivity-dashboard.png"
-        ).exists(), "Awesome Team Dashboard PNG was not extracted"
+def test_weekly_report_image_generation(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "images-only",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    assert Path(
+        f"{OUTPUT_DIR}/my-awesome-team-team-productivity-dashboard.png"
+    ).exists()
+
+
+def test_weekly_report_markdown(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "markdown",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    files = list(Path(OUTPUT_DIR).rglob("*"))
+    assert 1 == len(files)
+    assert Path(
+        f"{OUTPUT_DIR}/my-awesome-team-team-productivity-dashboard-with-images.md"
+    ).exists()
+
+
+def test_weekly_report_html(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "html",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    files = list(Path(OUTPUT_DIR).rglob("*"))  # Recursively get all files and folders
+    assert 1 == len(files)
+    assert Path(
+        f"{OUTPUT_DIR}/my-awesome-team-team-productivity-dashboard-with-images.html"
+    ).exists()
+
+
+def test_weekly_report_html_with_refs(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "html-with-refs",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    files = list(Path(OUTPUT_DIR).rglob("*"))
+    assert 7 == len(files)
+    assert Path(
+        f"{OUTPUT_DIR}/my-awesome-team-team-productivity-dashboard-with-image-refs.html"
+    ).exists()
+
+
+def test_weekly_report_markdown_with_refs(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "markdown-with-refs",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    files = list(Path(OUTPUT_DIR).rglob("*"))
+    assert 7 == len(files)
+    assert Path(
+        f"{OUTPUT_DIR}/my-awesome-team-team-productivity-dashboard-with-image-refs.md"
+    ).exists
+
+
+def test_weekly_report_console(setup_cli_isolated_env):
+    runner = setup_cli_isolated_env
+
+    result = runner.invoke(
+        cli,
+        [
+            *BASE_ARGS,
+            "--output-type",
+            "console",
+        ],
+    )
+
+    assert result.exit_code == 0, result.output
+    files = list(Path(OUTPUT_DIR).rglob("*"))
+    assert 0 == len(files)
+    assert 234374 == len(result.output)
--- tests/test_pdf_extract.py
+++ tests/test_pdf_extract.py
@@ -1,36 +1,22 @@
 import pathlib
-import unittest
 
-from logilica_weekly_report.pdf_extract import get_pdf_objects
+from logilica_weekly_report.pdf_extract import PDFExtract
 
 
-class MyTestCase(unittest.TestCase):
-    def test_get_pdf_objects(self):
-        config = {
-            "Mock Team": {
-                "team_dashboards": {
-                    "Mock Team Dashboard": {"filename": "sample_report.pdf"}
-                }
+def test_get_pdf_objects():
+    config = {
+        "Mock Team": {
+            "team_dashboards": {
+                "Mock Team Dashboard": {"filename": "sample_report.pdf"}
             }
         }
+    }
 
-        result = get_pdf_objects(config, pathlib.Path(__file__).parent / "fixtures")
-        self.assertEqual(
-            1,
-            len(result),
-            "Unexpected number of teams found.",
-        )
-        self.assertEqual(
-            1,
-            len(result["Mock Team"]),
-            "Unexpected number of dashboards found.",
-        )
-        self.assertEqual(
-            175526,
-            len(result["Mock Team"]["Mock Team Dashboard"]),
-            "Unexpected images size found.",
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+    result = PDFExtract().get_pdf_objects(
+        config, pathlib.Path(__file__).parent / "fixtures"
+    )
+    assert 1 == len(result), "Unexpected number of teams found."
+    assert 1 == len(result["Mock Team"]), "Unexpected number of dashboards found."
+    assert 175655 == len(
+        result["Mock Team"]["Mock Team Dashboard"]
+    ), "Unexpected image length"
